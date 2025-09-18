5689fe969a6f0b824c8f37d6dba2b0ee749a2f98
Raewon12 <fodnjs15@naver.com>
2025-09-18 12:44:53 +0900

    comot

--- shop_v1/shop_v1.py
+++ shop_v1/shop_v1.py
@@ -1,26 +1,17 @@
-#pip install pymysql
 import pymysql
 from dotenv import load_dotenv
-import os 
-load_dotenv()
-# 1 .DB연결
-conn = pymysql.connect(
-    host = os.getenv('DB_HOST'),
-    user = os.getenv('DB_USER'),
-    password = os.getenv('DB_PASSWORD'),
-    database=os.getenv('DB_NAME')
-)
-# 1 .DB연결
-conn = pymysql.connect(
-    host = '127.0.0.1',
-    user = 'root',
-    password = '1234',
-    database='mydb'
+import os
 
+load_dotenv()
+
+conn = pymysql.connect(
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME')
 )
 print('접속성공')
 conn.close()
-
 # 2. 각 테이블별 CRUD
 # C - insert 
 # R - select
